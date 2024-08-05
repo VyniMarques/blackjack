f0873993bddbc8247583647f7258c74dbf36d78c
Vinícius Marques <vinicius.ximenes@ges.inatel.br>
2024-08-05 20:11:03 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -66,16 +66,6 @@ def dealer_play(dealer):
     return int(total)
 
 
-def start_game():
-    global start_hand, dealer_hand
-    start_hand = deal(cards)
-    dealer_hand = deal(cards)
-    update_display()
-    result_label.config(text="")
-    hit_button.config(state=NORMAL)
-    stand_button.config(state=NORMAL)
-
-
 def update_display(show_full_dealer_hand=False):
     player_hand_label.config(
         text=f"Player Hand: {start_hand} = {sum_cards(start_hand)}"
@@ -87,22 +77,28 @@ def update_display(show_full_dealer_hand=False):
     else:
         dealer_hand_label.config(text=f"Dealer Hand: {dealer_hand[0]}, ?")
 
+    victories_label.config(text=f"Victories: {victories}")
+    defeats_label.config(text=f"Defeats: {defeats}")
+
 
 # Opção de comprar mais cartas
 def hit():
-    global start_hand
+    global start_hand, defeats
     start_hand = buy_card(start_hand)
     if sum_cards(start_hand) == 22:
         result_label.config(text="Estourou!")
+        defeats += 1
         update_display(show_full_dealer_hand=True)
         hit_button.config(state=DISABLED)
         stand_button.config(state=DISABLED)
+        update_display()
     else:
         update_display()
 
 
 # Opção de passar a vez / mostrar resultado
 def stand():
+    global victories, defeats
     dealer_result = dealer_play(dealer_hand)
     player_result = sum_cards(start_hand)
     update_display(show_full_dealer_hand=True)
@@ -110,22 +106,39 @@ def stand():
     stand_button.config(state=DISABLED)
     if dealer_result == 22:
         result_label.config(text="Dealer estourou! Você ganhou!")
+        victories += 1
     elif player_result == 22:
         result_label.config(text="Você estourou!")
+        defeats += 1
     elif dealer_result > player_result:
         result_label.config(text="Dealer ganhou")
+        defeats += 1
     elif dealer_result < player_result:
         result_label.config(text="Você ganhou")
+        victories += 1
     else:
         result_label.config(text="Empate")
+    update_display()
 
 
+def start_game():
+    global start_hand, dealer_hand, victories, defeats
+    start_hand = deal(cards)
+    dealer_hand = deal(cards)
+    update_display()
+    result_label.config(text="")
+    hit_button.config(state=NORMAL)
+    stand_button.config(state=NORMAL)
+
+
+victories = 0
+defeats = 0
 # ============================== Interface gráfica ==============================
 root = Tk()
 root.title("Blackjack ♠️♥️♦️♣️")
 root.geometry("500x300")
 
-photo = PhotoImage(file = 'cards.png')
+photo = PhotoImage(file="cards.png")
 root.wm_iconphoto(False, photo)
 
 style = ttk.Style()
@@ -144,14 +157,21 @@ dealer_hand_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="
 result_label = ttk.Label(root, text="")
 result_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
+victories_label = ttk.Label(root, text="Victories: ")
+victories_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+
+defeats_label = ttk.Label(root, text="Defeats: ")
+defeats_label.grid(row=3, column=1, columnspan=2, padx=10, pady=10, sticky="nsew")
+
+
 start_button = ttk.Button(root, text="Start Game", command=start_game)
-start_button.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
+start_button.grid(row=4, column=0, padx=10, pady=10, sticky="nsew")
 
 hit_button = ttk.Button(root, text="Hit", command=hit)
-hit_button.grid(row=4, column=0, padx=10, pady=10, sticky="nsew")
+hit_button.grid(row=5, column=0, padx=10, pady=10, sticky="nsew")
 
 stand_button = ttk.Button(root, text="Stand", command=stand)
-stand_button.grid(row=4, column=1, padx=10, pady=10, sticky="nsew")
+stand_button.grid(row=5, column=1, padx=10, pady=10, sticky="nsew")
 
 # Configurar a grade para expandir conforme necessário
 root.grid_columnconfigure(0, weight=1)
